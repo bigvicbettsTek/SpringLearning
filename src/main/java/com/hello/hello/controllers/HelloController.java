ea8b847b2e1e1176d2bddcead32d72e5b1470a3d
Betts <vbetts@teksystems.com>
2020-11-02 16:19:58 -0600

    Initial commit

---
+++ src/main/java/com/hello/hello/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.hello.hello.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String getHello() {
+        return "Hello World";
+    }
+
+    @PostMapping
+    public String postHello(@RequestBody String name) {
+        return "Hello " + name;
+    }
+}
