5bc2be48a2fcda87ad9e21faa9e222d3bac5e376
Betts <vbetts@teksystems.com>
2020-11-02 16:47:38 -0600

    Added Google Formatter to POM -Try #1

--- src/main/java/com/hello/hello/controllers/HelloController.java
+++ src/main/java/com/hello/hello/controllers/HelloController.java
@@ -3,15 +3,4 @@ package com.hello.hello.controllers;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class HelloController {
-
-    @GetMapping
-    public String getHello() {
-        return "Hello World";
-    }
-
-    @PostMapping
-    public String postHello(@RequestBody String name) {
-        return "Hello " + name;
-    }
-}
+public class HelloController { @GetMapping public String getHello() { return "Hello World"; }@PostMapping public String postHello(@RequestBody String name) { return "Hello " + name; }}
