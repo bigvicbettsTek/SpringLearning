fd9bcb019ebe8467439edf40a70c9c561d0d0a22
Betts <vbetts@teksystems.com>
2020-11-05 10:31:51 -0600

    Changed a bunch of stuff

--- src/main/java/com/hello/hello/Ct.java
+++
@@ -1,9 +0,0 @@
-package com.hello.hello;
-
-public class Ct {
-
-  public int d(int a, int b) {
-    if (b == 0) return Integer.MAX_VALUE;
-    else return a / b;
-  }
-}
--- src/main/java/com/hello/hello/controllers/HelloController.java
+++ src/main/java/com/hello/hello/controllers/HelloController.java
@@ -1,6 +1,5 @@
 package com.hello.hello.controllers;
 
-import java.util.Random;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,14 +13,5 @@ public class HelloController {
   @PostMapping
   public String postHello(@RequestBody String name) {
     return "Hello " + name;
-  }
-
-  public Boolean returnNull() {
-    return null;
-  }
-
-  String generateSecretToken() {
-    Random r = new Random();
-    return Long.toHexString(r.nextLong());
   }
 }
